b0424c74e87f5650ee63f10d0a17afe98ebf3a40
Darcksac <148557358+Darcksac@users.noreply.github.com>
2026-07-22 21:45:03 -0600

    fix: use alter true in sequelize sync to ensure expiresAt is added to production db

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ alter: true }).then(() => {
   console.log('Database connected and synced');
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
